5af4608f7838c0fff924905f11f6168472d9c3eb
Shashank Dhyani <shash.dhyani@gmail.com>
2018-08-13 20:43:10 +0530

    First Commit Launch
    Layout Page Responsive and index page

---
+++ PortfolioWebApplication/Content/js/custom.js
@@ -0,0 +1,18 @@
+//(function($){
+    "use strict"; // Start of use strict
+   
+    //back to top
+    $(window).scroll(function () {
+        if ($(this).scrollTop() !== 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').on("click", function () {
+        $("html, body").animate({scrollTop: 0}, 600);
+        return false;
+    });
+
+
+//})(jQuery);
